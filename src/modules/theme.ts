1892332789197663843ef9c2824dbae32697c0f0
Sooyoung <dannylisa67@gmail.com>
2021-06-26 01:41:22 +0900

    Commit1

--- src/modules/theme.ts
+++ src/modules/theme.ts
@@ -1,7 +1,7 @@
-import { dark, light } from "@/style/themes";
+import { dark_orange, light_orange } from "@/style/themes";
 import { DefaultTheme } from "@/style/styled";
 
-type themeType = "theme/light" | "theme/dark";
+type themeType = "theme/light/orange" | "theme/dark/orange";
 interface changeThemeAction {
     type: themeType;
 }
@@ -11,11 +11,11 @@ export const actionCreators = {
     changeTheme
 }
 
-// const theme = (state:DefaultTheme = dark, action:changeThemeAction) => {
-const theme = (state:DefaultTheme = light, action:changeThemeAction) => {
+// const theme = (state:DefaultTheme = dark_orange, action:changeThemeAction) => {
+const theme = (state:DefaultTheme = light_orange, action:changeThemeAction) => {
     switch(action.type){
-        case "theme/light": return light;
-        case "theme/dark": return dark;
+        case "theme/light/orange": return light_orange;
+        case "theme/dark/orange": return dark_orange;
         default: return state;
     }
 }
