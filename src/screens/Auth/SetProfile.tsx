30b5e73085e34ee2997c1083e929dd75a5da72b5
CreateValueFor <80185149+CreateValueFor@users.noreply.github.com>
2021-07-11 02:52:17 +0900

    Need To fix several...

--- src/screens/Auth/SetProfile.tsx
+++ src/screens/Auth/SetProfile.tsx
@@ -1,12 +1,46 @@
 import { GlobalState } from '@/modules'
+import useTheme from '@/modules/theme/hooks'
 import { DefaultTheme } from '@/style/styled'
-import React, { useState } from 'react'
-import { StyleSheet, View } from 'react-native'
-import { SafeAreaView } from 'react-navigation'
+import React, { useCallback, useEffect, useState } from 'react'
+import { StyleSheet, View, SafeAreaView } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
+import TextInput from '@/components/materials/TextInput'
+import TextButton from '@/components/materials/TextButton'
+import Text from '@/components/materials/Text'
+import { FlatList } from 'react-native-gesture-handler'
+import { useProfile, useUserState } from '@/modules/auth/hooks'
+import { UserProfile } from '@/modules/auth/reducer'
 
-function SetProfile({ route }: SetProfileProps) {
-  const { theme } = useSelector((state: GlobalState) => state)
+type UserDetailInfoProps = {
+  id: number
+  text: string
+}
+
+const UserDetailInfo: UserDetailInfoProps[] = [
+  {
+    id: 0,
+    text: '닉네임 설정',
+  },
+  {
+    id: 1,
+    text: '성별',
+  },
+  {
+    id: 2,
+    text: '이메일',
+  },
+  {
+    id: 3,
+    text: '전화번호',
+  },
+  {
+    id: 4,
+    text: '주소',
+  },
+]
+
+function SetProfile({ route }) {
+  const theme = useTheme()
   const {
     container,
     categoryView,
@@ -17,102 +51,62 @@ function SetProfile({ route }: SetProfileProps) {
     confirmButton,
   } = styles(theme)
   // const {username, profile:{address, nickname, tel, email, gender}} = useSelector((state: GlobalState) => state.userAuth)
-  const dispatch = useDispatch()
   const { text } = route.params
-
+  const { profile } = useUserState()
+  const { setProfile } = useProfile()
+  //profile 못 불러왔을 경우 대비해서 진행
+  if (!profile) return
+  const [userDetail, setUserDetail] = useState<UserProfile | null>(profile)
   //프로필 정보
-  // const [address, setAddress] = useState<string>('')
-  // const [nickname, setNickname] = useState<string>('')
-  // const [tel, setTel] = useState<string>('')
-  // const [email, setEmail] = useState<string>('')
-  // const [gender, setGender] = useState<string>('')
+  const [address, setAddress] = useState<string>('')
+  const [nickname, setNickname] = useState<string>('')
+  const [tel, setTel] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  //기본 값 null 설정하고 싶은데 이럴 경우 타입 지정 어떻게 하는지
+  const [gender, setGender] = useState<'M' | 'F'>('M')
 
   //프로필 정보 불러오기
-  // useEffect(() => {
-  //   setAddress(address)
-  //   setNickname(nickname)
-  //   setTel(tel)
-  //   setEmail(email)
-  //   setGender(gender)
-  // }, [])
+  useEffect(() => {
+    setAddress(profile.address)
+    setNickname(profile.nickname)
+    setTel(profile.tel)
+    setEmail(profile.email)
+    setGender(profile.gender)
+  }, [])
 
-  // const onPress = useCallback(() => {
-  //   // dispatch(setProfile({ address, nickname, tel, email, gender, token }))
-  //   setAddress('')
-  //   setNickname('')
-  //   setTel('')
-  //   setEmail('')
-  //   setGender('')
-  // }, [profile])
+  const onPress = useCallback(() => {
+    const profile: UserProfile = {
+      nickname,
+      gender,
+      tel,
+      email,
+      address,
+    }
+    // setProfile(profile)
+    // setAddress('')
+    // setNickname('')
+    // setTel('')
+    // setEmail('')
+    // setGender('M')
+  }, [profile])
   return (
     <SafeAreaView style={container}>
-      <View style={categoryContainer}>
-        {/* <View style={categoryView}>
-          <DefaultText text={'주소'} />
-          <TextInput
-            style={input}
-            onChangeText={(text) => setAddress(text)}
-            value={address}
-          />
+      <View style={categoryView}>
+        <Text content={'닉네임'} align={'left'} />
+        <TextInput onChangeText={setNickname} value={nickname} />
+        <Text content={'성별'} />
+        <View style={genderButtonContainer}>
+          <TextButton onPress={() => setGender('M')} content={'M'}></TextButton>
+          <TextButton onPress={() => setGender('F')} content={'F'}></TextButton>
         </View>
-        <View style={categoryView}>
-          <DefaultText text={'닉네임'} />
-          <TextInput
-            style={input}
-            onChangeText={setNickname}
-            value={nickname}
-          />
-        </View>
-        <View style={categoryView}>
-          <DefaultText text={'전화번호'} />
-          <TextInput
-            style={input}
-            onChangeText={(text) => setTel(text)}
-            value={tel}
-          />
-        </View>
-        <View style={categoryView}>
-          <DefaultText text={'이메일'} />
-          <TextInput
-            style={input}
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-          />
-        </View>
-        <View style={categoryView}>
-          <DefaultText text={'성별'} />
-          <View style={genderButtonContainer}>
-            <TouchableView
-              viewStyle={[
-                genderButton,
-                { backgroundColor: theme.content },
-                gender === 'M' && { backgroundColor: theme.selected },
-              ]}
-              style={genderButton}
-              onPress={() => {
-                setGender('M')
-              }}
-            >
-              <Text>M</Text>
-            </TouchableView>
-            <TouchableView
-              viewStyle={[
-                genderButton,
-                { backgroundColor: theme.content },
-                gender === 'F' && { backgroundColor: theme.selected },
-              ]}
-              onPress={() => {
-                setGender('F')
-              }}
-            >
-              <Text>F</Text>
-            </TouchableView>
-          </View>
-        </View>
-        <TouchableView onPress={onPress} viewStyle={confirmButton}>
-          <Text>{text}</Text>
-        </TouchableView> */}
+        <Text content={'이메일'} />
+        <TextInput onChangeText={setEmail} value={email} />
+        <Text content={'전화번호'} />
+        <TextInput onChangeText={setTel} value={tel} />
+        <Text content={'주소'} />
+        <TextInput onChangeText={setAddress} value={address} />
       </View>
+      <TextButton onPress={onPress} content={'완료'} />
     </SafeAreaView>
   )
 }
